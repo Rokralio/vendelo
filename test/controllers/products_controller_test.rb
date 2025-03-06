43d6c4c862f2098f44f6bf3a4ba1c93374d3d88a
Rokralio <rjgonzalez.1996@gmail.com>
2025-03-06 14:25:57 -0500

    [TEST] Add integration tests for ProductsController
      - Add 'products_controller_test.rb' to test the renderingof the
        products list and product detail pages.
      - Create fixture in 'products.yml' to provide test data for PS4 and
        Nintengo Switch products.
      - Tests veify that:
        - The products list page renders correctly and display 2 products.
        - The product detail page for the PS4 renders the correct tittle,
          description, and price.
        - Fixtures include sample data for PS4 and Nintendo Switch products.
    
      this ensure that the ProductsController behaves as expected and that
      the views display the correct information.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,12 +5,17 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path
+
     assert_response :success
+    assert_select '.product', 2
   end
 
   test 'render a detail product page' do
-    product = Product.create(name: 'Product 1', price: 100)
-    get product_path(product.id)
+    get product_path(products(:PS4))
+
     assert_response :success
+    assert_select '.tittle', 'PS4 Fat'
+    assert_select '.description', 'PS4 en buen estado'
+    assert_select '.price', '150'
   end
 end
