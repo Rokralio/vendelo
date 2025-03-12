4bbd7f5fec98e5036cd731a6e723422512bd5b31
Rokralio <rjgonzalez.1996@gmail.com>
2025-03-12 11:24:24 -0500

    [FEAT] migration and test for null constraints on product fields
      - Add a migration to enforce `null: false` constraints on `title`, `description`, and `price` fields in the `products` table.
      - Add a test to verify that creating a product with empty fields returns an `unprocessable_entity` response.
    
    These changes ensure data integrity by preventing the creation of products with missing required fields.

---
+++ db/migrate/20250312160400_add_null_false_to_product_fields.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNullFalseToProductFields < ActiveRecord::Migration[7.2]
+  def change
+    change_column_null(:products, :title, false)
+    change_column_null(:products, :description, false)
+    change_column_null(:products, :price, false)
+  end
+end
