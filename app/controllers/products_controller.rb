d530799c5e49ca6b8fb697b7e9bfdebd259c774f
Rokralio <rjgonzalez.1996@gmail.com>
2025-03-12 16:57:15 -0500

    [FEAT] photo upload functionality to products
      - Install Active Storage and run migrations to enable file attachments.
      - Add `has_one_attached :photo` to the `Product` model.
      - Add `:photo` to the permitted parameters in `ProductsController`.
      - Update the `_form.html.erb` partial to include a file input for photo uploads.
      - Display product photos in the `index` and `show` views.
    
    These changes allow users to upload and display photos for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :price, :photo)
   end
 end
