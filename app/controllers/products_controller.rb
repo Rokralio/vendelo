efc1eece436df68bd1a373663ef8519ca78c6cec
Rokralio <rjgonzalez.1996@gmail.com>
2025-03-12 14:37:34 -0500

    [FEAT] delete functionality for products
      - Add the `destroy` action in `ProductsController` to handle product deletion.
      - Add a link to delete products in the `show.html.erb` view with a confirmation dialog.
      - Add a route for deleting products (`delete '/products/:id'`).
      - Add a test to verify product deletion and flash message.
    
    These changes allow users to delete products and provide feedback on the deletion status.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,6 +37,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to(products_path, status: :see_other, notice: 'Tu producto ha sido eliminado exitosamente')
+  end
+
   private
 
   def product_params
